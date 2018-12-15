a8fd4b163e3a9b8c2e4e3c0b07446c942da3177b
itcast <bos@itcast.cn>
2018-12-15 16:30:10 +0800

    jack 提交了

---
+++ maven1-model/src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * @Auther: fang
+ * @Date: 2018/12/12 20:01
+ * @Description:
+ */
+public class HelloWorld {
+    public void sayHello(){
+        System.out.println("hello");
+    };
+}
---
+++ maven1-model/src/test/java/TestHelloWorld.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+/**
+ * @Auther: fang
+ * @Date: 2018/12/12 20:03
+ * @Description:
+ */
+public class TestHelloWorld {
+
+    @Test
+    public void testSayHello(){
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.sayHello();
+    };
+}
